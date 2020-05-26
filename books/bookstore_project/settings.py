bab7f28d3c23c4accee1c15a7904fd7d4ed8ffc3
krptic07 <kumarabhishek22101997@gmail.com>
2020-05-26 15:20:42 +0530

    ch7

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -37,17 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     #Third-party
     'crispy_forms',
+    'allauth',
+    'allauth.account',
 
     #Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
 
-#django-crispy-forms
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -142,5 +144,28 @@ STATICFILES_FINDERS = [
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+
+
+#django-allauth config
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT = 'home'
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_UNIQUE_EMAIL = True
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- books/users/views.py
+++ books/users/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
-from django.urls import reverse_lazy
-from django.views import generic
+# from django.urls import reverse_lazy
+# from django.views import generic
 
-from .forms import CustomUserCreationForm
+# from .forms import CustomUserCreationForm
 
 # Create your views here.
 
-class SignupPageView(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+# class SignupPageView(generic.CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'signup.html'
